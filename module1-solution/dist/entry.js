2277d9d1d1bc55eaeba3cf137fade063452507f8
Joakim Chiem <joakim.chiem@gmail.com>
2016-09-16 17:13:10 +0300

    Finished first assignment

---
+++ module1-solution/dist/entry.js
@@ -0,0 +1,28 @@
+'use strict';
+
+(function () {
+    angular.module('MainApp', []).controller('MainController', function ($scope) {
+        var sc = $scope;
+
+        sc.resetValidator = function () {
+            $scope.validator = '';
+        };
+
+        sc.checkIfTooMuch = function () {
+            if ($scope.lunchMenu === '') {
+                $scope.validator = 'Please enter data first';
+            } else {
+                /* grabs all items and stores into array, filters out empty strings and counts the array */
+                var lunchItemCount = $scope.lunchMenu.split(',').filter(function (v) {
+                    return v != '';
+                }).length;
+
+                if (lunchItemCount > 3) {
+                    $scope.validator = 'Too much!';
+                } else {
+                    $scope.validator = 'Enjoy!';
+                }
+            }
+        };
+    });
+})();
